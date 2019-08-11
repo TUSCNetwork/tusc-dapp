b2b18000627c9333545c7c748abcf47fcd0bda26
Charles Barry <cwbwork@gmail.com>
2019-08-11 18:56:02 -0400

    added registration, reorganized code

---
+++ contract.js
@@ -0,0 +1,382 @@
+//ropsten  *****************************************
+const contract_address = "0x04861e6eb04889f0549d52be118e7c66e92ac9dc";
+const occ_contract_address = "0x75584b2bfa5eb391c8234abcfb4cde5c07e2cf30";
+
+const erc20_abi = [
+{
+    "constant": true,
+    "inputs": [],
+    "name": "name",
+    "outputs": [
+    {
+        "name": "",
+        "type": "string"
+    }
+    ],
+    "payable": false,
+    "type": "function"
+},
+{
+    "constant": false,
+    "inputs": [
+    {
+        "name": "_spender",
+        "type": "address"
+    },
+    {
+        "name": "_value",
+        "type": "uint256"
+    }
+    ],
+    "name": "approve",
+    "outputs": [
+    {
+        "name": "success",
+        "type": "bool"
+    }
+    ],
+    "payable": false,
+    "type": "function"
+},
+{
+    "constant": true,
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [
+    {
+        "name": "",
+        "type": "uint256"
+    }
+    ],
+    "payable": false,
+    "type": "function"
+},
+{
+    "constant": false,
+    "inputs": [
+    {
+        "name": "_from",
+        "type": "address"
+    },
+    {
+        "name": "_to",
+        "type": "address"
+    },
+    {
+        "name": "_value",
+        "type": "uint256"
+    }
+    ],
+    "name": "transferFrom",
+    "outputs": [
+    {
+        "name": "success",
+        "type": "bool"
+    }
+    ],
+    "payable": false,
+    "type": "function"
+},
+{
+    "constant": true,
+    "inputs": [],
+    "name": "decimals",
+    "outputs": [
+    {
+        "name": "",
+        "type": "uint256"
+    }
+    ],
+    "payable": false,
+    "type": "function"
+},
+{
+    "constant": true,
+    "inputs": [],
+    "name": "version",
+    "outputs": [
+    {
+        "name": "",
+        "type": "string"
+    }
+    ],
+    "payable": false,
+    "type": "function"
+},
+{
+    "constant": true,
+    "inputs": [
+    {
+        "name": "_owner",
+        "type": "address"
+    }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+    {
+        "name": "balance",
+        "type": "uint256"
+    }
+    ],
+    "payable": false,
+    "type": "function"
+},
+{
+    "constant": true,
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [
+    {
+        "name": "",
+        "type": "string"
+    }
+    ],
+    "payable": false,
+    "type": "function"
+},
+{
+    "constant": false,
+    "inputs": [
+    {
+        "name": "_to",
+        "type": "address"
+    },
+    {
+        "name": "_value",
+        "type": "uint256"
+    }
+    ],
+    "name": "transfer",
+    "outputs": [
+    {
+        "name": "success",
+        "type": "bool"
+    }
+    ],
+    "payable": false,
+    "type": "function"
+},
+{
+    "constant": false,
+    "inputs": [
+    {
+        "name": "_spender",
+        "type": "address"
+    },
+    {
+        "name": "_value",
+        "type": "uint256"
+    },
+    {
+        "name": "_extraData",
+        "type": "bytes"
+    }
+    ],
+    "name": "approveAndCall",
+    "outputs": [
+    {
+        "name": "success",
+        "type": "bool"
+    }
+    ],
+    "payable": false,
+    "type": "function"
+},
+{
+    "constant": true,
+    "inputs": [
+    {
+        "name": "_owner",
+        "type": "address"
+    },
+    {
+        "name": "_spender",
+        "type": "address"
+    }
+    ],
+    "name": "allowance",
+    "outputs": [
+    {
+        "name": "remaining",
+        "type": "uint256"
+    }
+    ],
+    "payable": false,
+    "type": "function"
+},
+{
+    "inputs": [
+    {
+        "name": "_initialAmount",
+        "type": "uint256"
+    },
+    {
+        "name": "_tokenName",
+        "type": "string"
+    },
+    {
+        "name": "_decimalUnits",
+        "type": "uint8"
+    },
+    {
+        "name": "_tokenSymbol",
+        "type": "string"
+    }
+    ],
+    "type": "constructor"
+},
+{
+    "payable": false,
+    "type": "fallback"
+},
+{
+    "anonymous": false,
+    "inputs": [
+    {
+        "indexed": true,
+        "name": "_from",
+        "type": "address"
+    },
+    {
+        "indexed": true,
+        "name": "_to",
+        "type": "address"
+    },
+    {
+        "indexed": false,
+        "name": "_value",
+        "type": "uint256"
+    }
+    ],
+    "name": "Transfer",
+    "type": "event"
+},
+{
+    "anonymous": false,
+    "inputs": [
+    {
+        "indexed": true,
+        "name": "_owner",
+        "type": "address"
+    },
+    {
+        "indexed": true,
+        "name": "_spender",
+        "type": "address"
+    },
+    {
+        "indexed": false,
+        "name": "_value",
+        "type": "uint256"
+    }
+    ],
+    "name": "Approval",
+    "type": "event"
+},
+];
+const abi = [
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "last_occ_balance",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "tusc_address",
+                "type": "string"
+            }
+        ],
+        "name": "doSwap",
+        "outputs": [
+            {
+                "name": "success",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "occ_token",
+        "outputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "tokens_swapped",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "swaps_attempted",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "total_tokens",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "name": "_omega_address",
+                "type": "address"
+            },
+            {
+                "name": "_occ_contract_address",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    }
+];
---
+++ scripts.js
@@ -0,0 +1,101 @@
+let swap_server_ip = "http://18.218.117.148";
+let swap_server_port = "8080";
+let tusc_wallet_api = "/tusc/api/wallet/";
+let tusc_db_api = "/tusc/api/db/";
+let endpoint_list_account_balances = swap_server_ip + ":" + swap_server_port + tusc_wallet_api + "list_account_balances";
+let endpoint_register_account = swap_server_ip + ":" + swap_server_port + tusc_wallet_api + "register_account";
+let endpoint_suggest_brain_key = swap_server_ip + ":" + swap_server_port + tusc_wallet_api + "suggest_brain_key";
+let endpoint_swap_stats = swap_server_ip + ":" + swap_server_port + tusc_db_api + "swap_stats";
+let occ_precision = 18;
+let tusc_precision = 5;
+
+async function checkTuscAddress(tusc_address) {
+    var data = await $.getJSON(endpoint_list_account_balances + "/" + tusc_address.trim());
+    if (data.error){
+        return false;
+    } else if (data.result) {
+        if (data.result.length < 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
+
+async function getTuscAccountBalance(tusc_address) {
+    var data = await $.getJSON(endpoint_list_account_balances + "/" + tusc_address.trim());
+    if (data.error){
+        return -1;
+    } else if (data.result) {
+        if (data.result.length < 1) {
+            return -1;
+        } else {
+            return data.result[0].amount;
+        }
+    }
+}
+
+async function suggestBrainKey() {
+    var data = await $.getJSON(endpoint_suggest_brain_key);
+    if (data.error){
+        return data.error;
+    } else if (data.result) {
+        if (data.result.length < 1) {
+            return data.error;
+        } else {
+            return data.result;
+        }
+    }
+}
+
+async function getStats() {
+    if (document.getElementById("occ_left_to_swap") == null) {
+        return;
+    }
+    var data = await $.getJSON(endpoint_swap_stats);
+    //console.log(data)
+    if (data.error){
+        console.log(data);
+    } else {
+        document.getElementById("occ_left_to_swap").innerHTML = addDecimalPoint(data.occ_left_to_swap, occ_precision);
+        document.getElementById("occ_swapped").innerHTML = addDecimalPoint(data.occ_swapped, occ_precision);
+        document.getElementById("tusc_swapped").innerHTML = addDecimalPoint(data.tusc_swapped, tusc_precision);
+
+        calcEndofSwap(data.end_of_swap_date);
+        var x = setInterval(function() {calcEndofSwap(data.end_of_swap_date);}, 1000);
+    }
+}
+
+function addDecimalPoint(numberAsString, precision) {
+    // Add extra zeros on the left side if there aren't enough
+    while (precision > numberAsString.length - 1) {
+        numberAsString = "0" + numberAsString;
+    }
+
+    // Add a decimal point to the string so it's more human readible.
+    let decimalPlace = numberAsString.length - precision;
+
+    return [numberAsString.slice(0, decimalPlace), ".", numberAsString.slice(decimalPlace)].join('');
+}
+
+function calcEndofSwap(end_of_swap_date) {
+    var now = new Date().getTime();
+
+    var distance = new Date(end_of_swap_date) - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Get today's date and time
+
+    // Display the result in the element with id="demo"
+    document.getElementById("end_of_swap_date").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("end_of_swap_date").innerHTML = "The OCC to TUSC swap is complete. Swapping has been disabled.";
+    }
+}
